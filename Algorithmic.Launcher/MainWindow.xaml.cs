2f7d230fc9e0a8313f661fb6fcef7aeaea8e7f1b
SANGWOO PARK <cyberprophet@hanmail.net>
2022-11-16 00:56:52 +0900

    create environment that will work repeat

--- Algorithmic.Launcher/MainWindow.xaml.cs
+++ Algorithmic.Launcher/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ShareInvest
 {
@@ -19,7 +8,79 @@ namespace ShareInvest
     {
         public MainWindow()
         {
+            menu = new System.Windows.Forms.ContextMenuStrip
+            {
+                Cursor = System.Windows.Forms.Cursors.Hand
+            };
+            menu.Items.AddRange(new[]
+            {
+                new System.Windows.Forms.ToolStripMenuItem
+                {
+                    Name = nameof(Properties.Resources.LINK),
+                    Text = Properties.Resources.LINK
+                },
+                new System.Windows.Forms.ToolStripMenuItem
+                {
+                    Name = nameof(Properties.Resources.EXIT),
+                    Text = Properties.Resources.EXIT
+                }
+            });
+            menu.ItemClicked += (sender, e) =>
+            {
+                if (nameof(Properties.Resources.LINK).Equals(e.ClickedItem.Name,
+                                                             StringComparison.OrdinalIgnoreCase))
+                {
+
+
+                    return;
+                }
+                Visibility = Visibility.Hidden;
+
+                Close();
+            };
+            notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                ContextMenuStrip = menu,
+                Visible = true,
+                Text = Properties.Resources.TITLE,
+                Icon = Properties.Resources.RECYCLE,
+                BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+            };
+            notifyIcon.MouseDoubleClick += (sender, e) =>
+            {
+                if (IsVisible == false)
+                {
+                    Show();
+
+                    WindowState = WindowState.Normal;
+                }
+            };
+            Title = Properties.Resources.TITLE;
+
             InitializeComponent();
         }
+        void OnClosing(object sender, CancelEventArgs e)
+        {
+            if (MessageBoxResult.Cancel.Equals(MessageBox.Show(Properties.Resources.WARNING.Replace('|', '\n'),
+                                               Title,
+                                               MessageBoxButton.OKCancel,
+                                               MessageBoxImage.Warning,
+                                               MessageBoxResult.Cancel)))
+            {
+                e.Cancel = true;
+
+                return;
+            }
+            GC.Collect();
+        }
+        void OnStateChanged(object sender, EventArgs e)
+        {
+            if (WindowState.Normal != WindowState)
+            {
+                Hide();
+            }
+        }
+        readonly System.Windows.Forms.ContextMenuStrip menu;
+        readonly System.Windows.Forms.NotifyIcon notifyIcon;
     }
 }
